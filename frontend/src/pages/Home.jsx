09d6999129f347394cc467d7a167ee9a447ec98a
Akansha Jha <121955578+dummyDoona@users.noreply.github.com>
2024-03-14 00:23:28 +0530

    Add files via upload

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../index.css';
+import Saajha from '../components/Saajha';
+
+function Home (){
+
+    return (
+        <>
+        {/* Version 2 */}
+        <Saajha/>
+        
+
+        
+        </>
+    );
+}
+export default Home;
