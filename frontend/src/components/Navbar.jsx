93596c63bfd6a82d44cdf13dbca62867fa54990d
Akansha Jha <121955578+dummyDoona@users.noreply.github.com>
2024-03-14 00:17:12 +0530

    Create Navbar.jsx

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
+import '../index.css'
+
+export const Navbar = () => {
+  return (
+    <nav>
+        <Link to='/' className='nav-title'>
+            Saajha
+        </Link>
+        <ul>
+            <li>
+                <NavLink to='/'>About</NavLink>
+            </li>
+            <li>
+                <NavLink to='/NGOs'>NGOs</NavLink>
+            </li>
+            <li>
+                <NavLink to='/FAQs'>FAQs</NavLink>
+            </li>
+            <li>
+                <NavLink to='/LoginSignup'>Login/SignUp</NavLink>
+            </li>
+        </ul>
+    </nav>
+  )
+}
